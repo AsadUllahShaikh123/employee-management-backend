c0064f72a7a2e4cf720d569f859dba99ae5c9e28
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2023-10-31 23:42:10 +0500

    added EmployeeRepository

---
+++ src/main/java/com/spring_boot/employee_management/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.spring_boot.employee_management.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.spring_boot.employee_management.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+
+	
+}
